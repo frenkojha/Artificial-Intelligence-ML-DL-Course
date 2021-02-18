0de754451e069f1f7f751ed8e976d7e8cd81e389
frenko jha <64513064+frenkojha@users.noreply.github.com>
2021-02-18 09:03:16 +0530

    Update MLPractical04.py
    add comments

--- Python Programming/MLPractical04.py
+++ Python Programming/MLPractical04.py
@@ -1,15 +1,19 @@
+# Provide headings in the row data set 
+
 import pandas
 pandas.set_option('display.width', 1000)
 pandas.set_option('display.max_column', 9)
 
 filename = 'indians-diabetes.data.csv'
 
+# hnames denots for the heading of the data
 hnames = ['preg', 'plas', 'pres',
           'skin', 'test','mass',
           'pedi', 'age', 'class']
 
 df = pandas.read_csv(filename, names = hnames)
 
+# df.shape use for the data shape like how manys rows and columns it content
 print( "pandas Data : " , df.shape  )
 
 print( df.dtypes )
